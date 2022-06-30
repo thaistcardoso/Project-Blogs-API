4ee303e26d99bb7381abdb08bcb447264c41c368
Thais Cardoso <thaistauane.cardoso@gmail.com>
2022-06-30 19:37:19 -0300

    [FEAT] - does developed route /user method post
    created route /user returning a token then a response

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -3,16 +3,15 @@ const express = require('express');
 const router = express.Router();
 
 const servicesUser = require('../services/servicesUser');
-const { authenticate } = require('../services/authService');
+// const { authenticate } = require('../services/authService');
 
 const { validateName } = require('../middlewares/validateName');
 const { validateEmail } = require('../middlewares/validateEmail');
 const { validatePassword } = require('../middlewares/validatePassword');
 
-router.post('/', validateName, validateEmail, validatePassword, async (req, res) => { 
-    await servicesUser.createUser(req.body);
-    const token = await authenticate(req.body);
-    res.status(201).json(token);
+router.post('/', validateEmail, validateName, validatePassword, async (req, res) => { 
+    const newUser = await servicesUser.createUser(req.body);
+    res.status(201).json(newUser);
 });
 
 module.exports = router;
--- src/services/servicesUser.js
+++ src/services/servicesUser.js
@@ -1,7 +1,22 @@
 const { User } = require('../database/models');
+const { generateJWToken } = require('../utils/jwt');
 // const { generateJWToken } = require('../utils/jwt');
 
-    const createUser = ({ displayName, email, password, image }) =>
-     User.create({ displayName, email, password, image });
+    const createUser = async ({ id, displayName, email, password, image }) => {
+        const mail = await User.findOne({
+            attributes: ['id', 'displayName', 'email', 'password', 'image'],
+            where: { email },
+        });
+        
+        if (mail) {
+            const error = new Error('User already registered');
+            error.status = 409;
+            throw error;
+        }
+        const newUser = await User.create({ id, displayName, email, password, image });
+        const token = generateJWToken(newUser.dataValues);
+
+        return { token };
+    };
 
 module.exports = { createUser };
